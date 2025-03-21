eb0352c2e12763dfd260f58d5aff98387e45a6e7
samuelklx <156188400+samuelklx@users.noreply.github.com>
2025-03-21 16:56:01 -0400

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,62 @@
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+	  <><Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Stock Sim | Home Page</title>
+    </Head>
+    <body>
+        <body>
+          <a href="index.html"> <img class="banner" src="LOGOv1.png" /></a>
+          <h1>Stock Trading System Simulator</h1>
+          <h2>By Team 21</h2>
+          <h2>Sam Kline, Michael Lacey, Josien Lajoie</h2>
+          <div class="navbar">
+            <a class="login" href="login.html">Log In</a>
+            <a href="viewschedule.html">View Schedule</a>
+            <a href="viewmarket.html">View Market</a>
+            <div class="dropdown">
+              <button class="dropbtn">Account</button>
+              <div class="dropdown-content">
+                <a href="profile.html">Profile</a>
+                <a href="portfolio.html">Portfolio</a>
+                <a href="transhistory.html">Transaction History</a>
+              </div>
+            </div>
+            <div class="dropdown">
+              /*!-- check if user is administrator to reveal Hidden attributes --*/
+              <button hidden class="dropbtn">Edit</button>
+              <div class="dropdown-content">
+                <a href="editmarket.html">Edit Market</a>
+                <a href="editschedule.html">Edit Schedule</a>
+              </div>
+            </div>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        
+
+        <h3>Home</h3>
+        <h4>Welcome To Sim Stock! Where Sim-ple Investments Score Big</h4>
+        <img id="homeimg" src="homepage.jpeg" />
+          <p id="hometxt">
+            Sim Stock is your one-stop shop for all of your investment needs, offering a intutive and ideal platform for managing your porfolio whenever needed and interacting
+            with the market during active hours. With our solution, you will be able to track all of your transactions on the market, review your stock holdings, and deposit and cashout
+            your balance all under one platform! Whether you are a new or experienced trader, Sim Stock holds all of the tools that will ensure your financial success and build confidence
+            to navigate the market efficiently with its user-friendly interface.
+
+          </p>
+
+          <h4 id="h4alt1">Ready To Start? </h4>
+          <img id="homeimg2" src="profileicon.png" />
+          <a href="signup.html"> <h4 id="h4alt2"> Sign Up For Free!</h4></a>
+          <img id="homeimg3" src="stockicon.png" />
+          <a href="viewmarket.html"> <h4 id="h4alt2"> View Our Current Market</h4></a>
+        </body>
+	</body>
+		</>
   );
 }
